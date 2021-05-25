8b3e45c79ede68c8fe37fe3dbaedbfebf2f71195
Ricardo Sanchez <ricsaniosdev@gmail.com>
2021-05-25 00:01:06 -0400

    Initial Commit

---
+++ baseArquitecture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  baseArquitecture
+//
+//  Created by Ricardo Sanchez on 5/25/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
